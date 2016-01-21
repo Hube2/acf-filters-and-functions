c50731f8e4dd5ed86c9a684ad9e503d6c15b736a
John A. Huebner II <Hube2@users.noreply.github.com>
2016-01-20 21:49:03 -0500

    added public post type location rule

---
+++ public-post-type-location-rule.php
@@ -0,0 +1,42 @@
+<?php 
+	
+	/* 
+		Custom location rule to add field group to any public post type
+	*/
+	
+	add_filter('acf/location/rule_values/post_type', 'acf_location_rules_values_public_post_type');
+		
+	function acf_location_rules_values_public_post_type($choices) {
+		if (!isset($choices['public-post-type'])) {
+			$choices['public-post-type'] = __('Public Post Type');
+		}
+		return $choices;
+	}
+	
+	add_filter('acf/location/rule_match/post_type', 'acf_location_rules_match_public_post_type', 10, 3);
+		
+	function match_public_post_type($match, $rule, $options) {
+		if ($rule['value'] != 'public-post-type') {
+			return $match;
+		}
+		if (isset($options['post_type'])) {
+			$post_type = $options['post_type'];
+		} else {
+			if (!isset($options['post_id'])) {
+				return false;
+			}
+			$post_type = get_post_type(intval($options['post_id']));
+		}
+		$post_type = get_post_type_object($options['post_type']);
+		if (!$post_type) {
+			return false;
+		}
+		if ($rule['operator'] == '==') {
+			$match = $post_type->public;
+		} elseif ($rule['operator'] == '!=') {
+			$match = !$post_type->public;
+		}
+		return $match;
+	}
+	
+?>
