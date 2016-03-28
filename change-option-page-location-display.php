bf69c4dac256378ef79079f89c89c9270993ec70
John A. Huebner II <Hube2@users.noreply.github.com>
2016-03-28 12:49:19 -0400

    added file

---
+++ change-option-page-location-display.php
@@ -0,0 +1,21 @@
+<?php 
+	
+	/*
+		ACF shows the menu_title as the choice for location rules
+		can be confusing when mutltiple options pages have the same menu title
+		this filter will change the display to the page title
+	*/
+	
+	add_filter('acf/location/rule_values/options_page', 'options_page_rule_values_titles', 20);
+	function options_page_rule_values_titles($choices) {
+		$pages = acf_get_options_pages();
+		if (!$pages) {
+			return;
+		}
+		foreach ($pages as $page) {
+			$choices[$page['menu_slug']] = $page['page_title'];
+		}
+		return $choices;
+	}
+	
+?>
