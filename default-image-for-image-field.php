3588401cd37548cd24dfbdc72de268cd5db4b094
John A. Huebner II <Hube2@users.noreply.github.com>
2016-04-10 12:14:35 -0400

    added comment

--- default-image-for-image-field.php
+++ default-image-for-image-field.php
@@ -2,6 +2,7 @@
   
   // add default image setting to ACF image fields
   // let's you select a defualt image
+  // this is simply taking advantage of a field setting that already exists
   
 	add_action('acf/render_field_settings/type=image', 'add_default_value_to_image_field', 20);
 	function add_default_value_to_image_field($field) {
