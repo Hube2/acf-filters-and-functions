80b71c2b4a0916f42ba01bd74bf40544dad04a7c
John A. Huebner II <Hube2@users.noreply.github.com>
2016-06-25 08:44:47 -0400

    removed translation, corrected text
    Translationscan be added by people using this and 'acf' text domain does not contain references to these strings.

--- default-image-for-image-field.php
+++ default-image-for-image-field.php
@@ -7,8 +7,8 @@
 	add_action('acf/render_field_settings/type=image', 'add_default_value_to_image_field', 20);
 	function add_default_value_to_image_field($field) {
 		acf_render_field_setting( $field, array(
-			'label'			=> __('Default Image ID','acf'),
-			'instructions'	=> __('Appears when creating a new post','acf'),
+			'label'			=> 'Default Image',
+			'instructions'		=> 'Appears when creating a new post',
 			'type'			=> 'image',
 			'name'			=> 'default_value',
 		));
